017c1b522251bb08446a4b92859a9d1b12bba850
sdas373 <33857833+sdas373@users.noreply.github.com>
2019-07-05 20:16:05 +1200

    Move project files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className='headerText'>Let's check! How Old Are You? ;) </h1>
+        </div>
+    )
+}
